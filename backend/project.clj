393b54fb8b6659d8f629645a9c7cd6052110eafb
miolfo <mikko.o.forsman@gmail.com>
2018-05-10 21:41:19 +0300

    Refactored namespace names, added dependency for hugsql

--- backend/project.clj
+++ backend/project.clj
@@ -1,12 +1,13 @@
-(defproject backend "0.1.0-SNAPSHOT"
+(defproject candystore-backend "0.1.0-SNAPSHOT"
   :description "Backend for candystore application"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
-                 [postgresql "9.3-1102.jdbc41"]]
+                 [postgresql "9.3-1102.jdbc41"]
+                 [com.layerware/hugsql "0.4.8"]]
   :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler backend.handler/app}
+  :ring {:handler candystore.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
--- backend/src/candystore/handler.clj
+++ backend/src/candystore/handler.clj
@@ -1,4 +1,4 @@
-(ns backend.handler
+(ns candystore.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
