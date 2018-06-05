21047cc87fa7173bff8ba3db6c369a8f70a0a45c
miolfo <mikko.o.forsman@gmail.com>
2018-06-05 21:05:29 +0300

    Users balance gets updated when adding transactions, refactored transaction api

--- backend/src/candystore/sql/users.sql
+++ backend/src/candystore/sql/users.sql
@@ -5,8 +5,20 @@
 SELECT * 
   FROM store_users;
 
--- :name get-user-by-id
+-- :name get-user-by-id :? :1
 -- :doc get user by id
 SELECT *
+  FROM store_users
+  WHERE id = :id;
+
+-- :name update-user-balance :! :n
+-- :doc update user blanace
+UPDATE store_users
+  SET balance = :balance
+  WHERE id = :id;
+
+-- :name get-user-balance :? :1
+-- :doc get single users balance
+SELECT balance
   FROM store_users
   WHERE id = :id;
