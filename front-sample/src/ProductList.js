9fe4978fa2ea9469bb4639e1484557152495141e
Matti Määttänen <matti.maatt@gmail.com>
2018-06-12 03:15:49 +0300

    added component to list products as base for front

---
+++ front-sample/src/ProductList.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+class ProductList extends React.Component {
+  constructor() {
+      super();
+      this.state = {
+          products: [],
+      };
+    }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/products/all', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json'
+      }
+    }).then(function(response) {
+      return response.json();
+    }, function(error) {
+      console.log("error: " + error.message);
+    }).then(data => {
+      let products = data.products;
+      console.log('data contained in response: ' + JSON.stringify(products));
+      let mappedProducts = products.map((product) => {
+        return(
+          <div key={product.id}>{product.name}</div>
+        )
+      })
+      console.log('mapped products: ' + JSON.stringify(mappedProducts));
+      this.setState({products: mappedProducts});
+    })
+    console.log("state " + this.state.products);
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h2>Product list for {this.props.name}</h2>
+        {this.state.products}
+      </div>
+    )
+  }
+}
+
+export default ProductList;
+
+{/*
+// export default class ProductList extends React.Component {
+//  render() {
+//        return (
+//            <div className="product-list">
+//                <h2> List of products for {this.props.name} </h2>
+//                <ul>
+//                    <li>Item1</li>
+//                    <li>Item2</li>
+//                    <li>Item3</li>
+//                </ul>
+//            </div>
+//        );
+//    }
+// */
+}
