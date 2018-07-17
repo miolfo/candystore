eed3ea30a5a480e01d05adf2ca63343aa661038e
miolfo <mikko.o.forsman@gmail.com>
2018-07-17 13:08:10 +0300

    Submitting transactions from the form to the backend

--- front-sample/src/components/Transaction.js
+++ front-sample/src/components/Transaction.js
@@ -5,20 +5,49 @@ const apiPort = process.env.REACT_APP_API_PORT || '3333';
 const apiEndpoint = '/transactions/';
 
 export default class Transaction extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      product_id: "",
+      user_id: "",
+      amount: ""
+    }
+
+    this.inputChanged = this.inputChanged.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   handleSubmit(event) {
     event.preventDefault();
-    const data = new FormData(event.target);
 
+    let body = {};
+    if(this.state.product_id !== "") {
+      body = {
+        user_id: parseInt(this.state.user_id),
+        product_id: parseInt(this.state.product_id)
+      }
+    } else {
+      body = {
+        user_id: parseInt(this.state.user_id),
+        amount: parseFloat(this.state.amount)
+      }
+    }
     fetch(apiUrl + ':' + apiPort + apiEndpoint, {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        user_id: 1,
-        product_id: 2
-      })
+      body: JSON.stringify(body)
+    });
+  }
+
+  inputChanged(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
     });
   }
 
@@ -28,19 +57,19 @@ export default class Transaction extends Component {
         <div>
         <label>
           Product id:
-          <input type="number" name="product_id" />
+          <input type="number" name="product_id" onChange={this.inputChanged} value={this.state.product_id}/>
         </label>
         </div>
         <div>
         <label>
           User id:
-          <input type="number" name="user_id"/>
+          <input type="number" name="user_id" onChange={this.inputChanged} value={this.state.user_id} required={true}/>
         </label>
         </div>
         <div>
         <label>
            Amount:
-          <input type="number" name="amount"/>
+          <input type="number" name="amount" onChange={this.inputChanged} value={this.state.amount}/>
         </label>
         </div>
         <input type="submit" value="Submit" />
